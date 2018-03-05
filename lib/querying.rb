b390d6c2c20f0fa4e59d1a6d0b5c6f4cca963134
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-05 19:37:56 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT Authors.name, Series.title
 FROM Authors
 LEFT JOIN Series
-ON Series.author_id = Authors.ide"
+ON Series.author_id = Authors.id"
 end
 
 def select_series_title_with_most_human_characters
