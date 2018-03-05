c228508fba94064ed0dfeac60cfabc95d1d53b37
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-05 19:34:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,9 @@ ORDER BY COUNT(Characters.species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  ""
+  "SELECT Authors.name, Subgenres.name
+FROM Authors, Subgenres
+GROUP BY Authors.name"
 end
 
 def select_series_title_with_most_human_characters
